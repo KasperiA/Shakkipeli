3ae61d86e3e00895ea8175e2b90e08994ff80a06
Kasperi Aavasaari <aavasaarikasperi@gmail.com>
2018-02-25 21:33:10 +0200

    Korjattu Issue #3

--- Shakkipeli/src/Torni.java
+++ Shakkipeli/src/Torni.java
@@ -12,7 +12,7 @@ class Torni extends Nappula{
 		/* Laudan ulkopuolelle ei voida siirtyä, 
 		 * Eikä voi liikkua vinoittain*/
 
-		if (x2 > 7 || x2 < 0 || y2 > 7 || y2 < 0 || ((x1 != x2 && y1 != y2) || (x1 != x2 && y1 != y2))) {
+		if (x2 > 7 || x2 < 0 || y2 > 7 || y2 < 0 || (x1 != x2 && y1 != y2)) {
 			return false;
 		}
 		
