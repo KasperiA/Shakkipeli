2312107510d4f6c5bda6d1dfe145bb9b0b8c09bd
Kasperi Aavasaari <aavasaarikasperi@gmail.com>
2018-02-28 22:13:28 +0200

    Korjattu pieni bugi

--- Shakkipeli/src/Sotilas.java
+++ Shakkipeli/src/Sotilas.java
@@ -20,10 +20,10 @@ class Sotilas extends Nappula {
 	public boolean liikkeenTarkistus(Shakkilauta lauta, int x1, int y1, int x2, int y2) {
 		
 		/* Mustien liikkeiden erotukset y1 - y2 aina positiiviset ja valkoisten negatiiviset. 
-		 * Täytyy tehdä kummallekkin värille omat liikkeenTarkistukset, koska muuten ei toimisi odotetusti
+		 * TÃ¤ytyy tehdÃ¤ kummallekkin omat, koska muuten ei toimisi odotetusti
 		 */
 		
-		/* Laudan ulkopuolelle ei voida siirtyä */
+		/* Laudan ulkopuolelle ei voida siirtyï¿½ */
 
 		if (x2 > 7 || x2 < 0 || y2 > 7 || y2 < 0) {
 			return false;
@@ -31,15 +31,15 @@ class Sotilas extends Nappula {
 		
 		
 		
-		/* Vain ensimmäisellä siirrolla voidaan liikkua kaksi ruutua eteenpäin samalla linjalla*/
+		/* Vain ensimmï¿½isellï¿½ siirrolla voidaan liikkua kaksi ruutua eteenpï¿½in samalla linjalla*/
 				
-		if ((!this.annaVari() && ensimmainenSiirto && x1 == x2 && y2 - y1 == 2 && lauta.annaRuutu(x2, y2) == null && lauta.annaRuutu(x2, y2 - 1) == null) 
-				|| (this.annaVari() && ensimmainenSiirto && x1 == x2 && y2 - y1 == -2 && lauta.annaRuutu(x2, y2) == null && lauta.annaRuutu(x2, y2 + 1) == null)) {
+		if ((!this.annaVari() && ensimmainenSiirto && x1 == x2 && y1 - y2 == 2 && lauta.annaRuutu(x2, y2) == null && lauta.annaRuutu(x2, y2 - 1) == null) 
+				|| (this.annaVari() && ensimmainenSiirto && x1 == x2 && y1 - y2 == -2 && lauta.annaRuutu(x2, y2) == null && lauta.annaRuutu(x2, y2 + 1) == null)) {
 			this.ensimmainenSiirto = false;
 			return true;
 		}
 		
-		/* Normaali yhden ruudun liike eteenpäin samalla linjalla*/
+		/* Normaali yhden ruudun liike eteenpï¿½in samalla linjalla*/
 		
 		if ((!this.annaVari() && x1 == x2 && lauta.annaRuutu(x2,y2) == null && y1 - y2 == 1)
 				|| (this.annaVari() && x1 == x2 && lauta.annaRuutu(x2,y2) == null && y1 - y2 == -1)) {
@@ -48,7 +48,7 @@ class Sotilas extends Nappula {
 		
 		
 		
-		/* Syöminen yhden ruudun verran oikealle tai vasemmalle*/
+		/* Syï¿½minen yhden ruudun verran oikealle tai vasemmalle*/
 		
 		if ((!this.annaVari() && x1 - x2 == 1 && y1 - y2 == 1 && lauta.annaRuutu(x2, y2).annaVari() != this.annaVari()) 
 				|| (!this.annaVari() && x1 - x2 == -1 && y1 - y2 == 1 && lauta.annaRuutu(x2, y2).annaVari() != this.annaVari())
