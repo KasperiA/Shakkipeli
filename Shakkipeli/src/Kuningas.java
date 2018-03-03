c144d2563472cf9fe8af67361be08c39ff635d9d
Kasperi Aavasaari <aavasaarikasperi@gmail.com>
2018-03-03 22:36:06 +0200

    Kuningas melkein valmis

--- Shakkipeli/src/Kuningas.java
+++ Shakkipeli/src/Kuningas.java
@@ -19,6 +19,67 @@ public class Kuningas extends Nappula{
 			return false;
 		}
 		
+		boolean nappulaVari = false;
+
+		try {
+			if (lauta.annaRuutu(x2, y2).annaVari() != this.annaVari()) {
+				nappulaVari = true;
+			}
+		} catch (Exception e) {
+			nappulaVari = false;
+		}
+		
+		boolean uhataankoRuutua = uhataankoRuutua(lauta, x2, y2);
+		
+		/* Kahdeksan ruutua ympärillä joihin voi liikkua, jos sitä ei uhata */
+		
+		if (!uhataankoRuutua && (lauta.annaRuutu(x2, y2) == null || nappulaVari)) {
+			if (x1 - x2 == 1 && y1 - y2 == 1) {
+				return true;
+			} else if (x1 == x2 && y1 - y2 == 1) {
+				return true;
+			} else if (x1 - x2 == -1 && y1 - y2 == 1) {
+				return true;
+			} else if (x1 - x2 == -1 && y1 == y2) {
+				return true;
+			} else if (x1 - x2 == -1 && y1 - y2 == -1) {
+				return true;
+			} else if (x1 == x2 && y1 - y2 == -1) {
+				return true;
+			} else if (x1 - x2 == 1 && y1 - y2 == -1) {
+				return true;
+			} else if (x1 - x2 == 1 && y1 == y2) {
+				return true;
+			}
+		}
+		
+		
+		
+		
+		return false;
+	}
+	
+	
+	// Palauttaa true, jos ruutua uhataan
+	public boolean uhataankoRuutua(Shakkilauta lauta, int x, int y) {
+		for (int sarake = 0; sarake < 8; sarake++) {
+			for(int rivi = 0; rivi < 8; rivi++) {
+				if (lauta.annaRuutu(sarake, rivi) instanceof Kuningas) {
+					continue;
+				}
+				System.out.println(sarake + ", " + rivi);
+				System.out.println("tyhjä: " + (lauta.annaRuutu(sarake, rivi) != null));
+				if(lauta.annaRuutu(sarake, rivi) != null) {
+					System.out.println("vari: " + (lauta.annaRuutu(sarake, rivi).annaVari() != this.annaVari()));
+					if(lauta.annaRuutu(sarake, rivi).annaVari() != this.annaVari()) {
+						System.out.println("ruutu: " + (lauta.annaRuutu(sarake, rivi).liikkeenTarkistus(lauta, sarake, rivi, x, y)));
+						if (lauta.annaRuutu(sarake, rivi).liikkeenTarkistus(lauta, sarake, rivi, x, y)) {
+							return true;
+						}
+					}
+				}
+			}
+		}
 		return false;
 	}
 }
