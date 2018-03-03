c2f462025e45d475b54798c7d5de6e7a96dbcb80
Kasperi Aavasaari <aavasaarikasperi@gmail.com>
2018-03-03 22:43:33 +0200

    Korjaa Issue #4

--- Shakkipeli/src/Hevonen.java
+++ Shakkipeli/src/Hevonen.java
@@ -1,7 +1,6 @@
 
-class Hevonen extends Nappula{
-	
-	
+class Hevonen extends Nappula {
+
 	public Hevonen(boolean vari) {
 		if (vari) {
 			super.asetaSymboli('H');
@@ -13,40 +12,42 @@ class Hevonen extends Nappula{
 
 	@Override
 	public boolean liikkeenTarkistus(Shakkilauta lauta, int x1, int y1, int x2, int y2) {
-		
+
 		/* Laudan ulkopuolelle ei voida siirtyä */
 
 		if (x2 > 7 || x2 < 0 || y2 > 7 || y2 < 0) {
 			return false;
 		}
-		
-		/*Yhdestä ruudusta on mahdollista siirtyä 8 eri ruutuun
-		 * Myötäpäivään tehty liikkeet */
-		
+
+		/*
+		 * Yhdestä ruudusta on mahdollista siirtyä 8 eri ruutuun Myötäpäivään
+		 * tehty liikkeet
+		 */
+
 		boolean nappulaVari = false;
-		
+
 		try {
-			if (lauta.annaRuutu(x2,y2).annaVari() != this.annaVari()) {
+			if (lauta.annaRuutu(x2, y2).annaVari() != this.annaVari()) {
 				nappulaVari = true;
 			}
 		} catch (Exception e) {
 			nappulaVari = false;
 		}
-		
-		if ((x1 - x2 == -1 && y1 - y2 == 2 && (nappulaVari || lauta.annaRuutu(x2,y2) == null))
-				||(x1 - x2 == -2 && y1 - y2 == 1 && (nappulaVari || lauta.annaRuutu(x2,y2) == null))
-				||(x1 - x2 == -2 && y1 - y2 == -1 && (nappulaVari || lauta.annaRuutu(x2,y2) == null))
-				||(x1 - x2 == -1 && y1 - y2 == -2 && (nappulaVari || lauta.annaRuutu(x2,y2) == null))
-				||(x1 - x2 == 1 && y1 - y2 == -2 && (nappulaVari || lauta.annaRuutu(x2,y2) == null))
-				||(x1 - x2 == 2 && y1 - y2 == -1 && (nappulaVari || lauta.annaRuutu(x2,y2) == null))
-				||(x1 - x2 == 2 && y1 - y2 == 1 && (nappulaVari || lauta.annaRuutu(x2,y2) == null))
-				||(x1 - x2 == 1 && y1 - y2 == 2 && (nappulaVari || lauta.annaRuutu(x2,y2) == null))) {
-					return true;
-				}
-		
-		
+
+		if (nappulaVari || lauta.annaRuutu(x2, y2) == null) {
+			if ((x1 - x2 == -1 && y1 - y2 == 2) 
+					|| (x1 - x2 == -2 && y1 - y2 == 1) 
+					|| (x1 - x2 == -2 && y1 - y2 == -1)
+					|| (x1 - x2 == -1 && y1 - y2 == -2) 
+					|| (x1 - x2 == 1 && y1 - y2 == -2)
+					|| (x1 - x2 == 2 && y1 - y2 == -1) 
+					|| (x1 - x2 == 2 && y1 - y2 == 1)
+					|| (x1 - x2 == 1 && y1 - y2 == 2)) {
+				return true;
+			}
+		}
+
 		return false;
 	}
-	
-	
+
 }
